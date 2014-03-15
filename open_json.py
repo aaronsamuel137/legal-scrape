cab743d48dd8e11f3ca271e7483e29b7ab6fe056
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-03-15 15:34:19 -0600

    add queue

--- open_json.py
+++ open_json.py
@@ -15,7 +15,9 @@ link_re = re.compile("http://www\.legis\.state\.pa\.us//WU01/LI/LI/CT/HTM/[0-9]+
 
 # obj = json.load(open('items.json'))
 
-r = requests.get('http://www.legis.state.pa.us/cfdocs/legis/LI/consCheck.cfm?txtType=HTM&ttl=01')
+# r = requests.get('http://www.legis.state.pa.us/cfdocs/legis/LI/consCheck.cfm?txtType=HTM&ttl=01')
+r = requests.get('http://www.legis.state.pa.us/cfdocs/legis/LI/consCheck.cfm?txtType=HTM&ttl=01&div=0&chpt=17')
 link = link_re.findall(r.text)[0].replace("'", '')
+print link
 r2 = requests.get(link)
 print r2.text
